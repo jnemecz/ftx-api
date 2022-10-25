aba77fb24226bdba204a9b20a6b1a3b6934baef5
jnemecz <jiri.nemec@gmail.com>
2022-10-25 21:00:59 +0200

    timeouty

--- src/main/java/com/akalea/ftx/configuration/FtxApiConfiguration.java
+++ src/main/java/com/akalea/ftx/configuration/FtxApiConfiguration.java
@@ -7,6 +7,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.DefaultResponseErrorHandler;
@@ -33,86 +35,111 @@ import java.util.stream.Collectors;
 @ComponentScan(basePackages = "com.akalea.ftx")
 public class FtxApiConfiguration {
 
-    @Autowired
-    private Environment env;
+  @Autowired
+  private Environment env;
 
-    public FtxApiConfiguration() {
-        CertUtils.acceptAllCertificates();
+  @Value("${app.rest.readTimout}")
+  private Integer readTimout;
+
+  @Value("${app.rest.connectTimeout}")
+  private Integer connectTimeout;
+
+  public FtxApiConfiguration() {
+    CertUtils.acceptAllCertificates();
+  }
+
+  public String getProperty(String property) {
+    return env.getProperty(property);
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper om =
+        Jackson2ObjectMapperBuilder
+            .json()
+            .build();
+    om.setSerializationInclusion(Include.NON_NULL);
+    return om;
+  }
+
+  @Bean
+  public RestTemplate restTemplate(ObjectMapper om) {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    if (readTimout > 0 || connectTimeout > 0) {
+
+      final SimpleClientHttpRequestFactory rf = new SimpleClientHttpRequestFactory();
+
+      if (readTimout > 0) {
+        rf.setReadTimeout(readTimout);
+      }
+
+      if (connectTimeout > 0) {
+        rf.setConnectTimeout(connectTimeout);
+      }
+
+      restTemplate.setRequestFactory(rf);
+
     }
 
-    public String getProperty(String property) {
-        return env.getProperty(property);
+    MappingJackson2HttpMessageConverter jacksonConverter =
+        new MappingJackson2HttpMessageConverter(om);
+
+    restTemplate.setMessageConverters(
+        restTemplate.getMessageConverters()
+            .stream()
+            .map(c -> c instanceof MappingJackson2HttpMessageConverter ? jacksonConverter : c)
+            .collect(Collectors.toList()));
+
+    if (env.acceptsProfiles("debug")) {
+      List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+      if (interceptors == null || interceptors.isEmpty()) {
+        interceptors = new ArrayList<>();
+      }
+      interceptors.add(new LoggingInterceptor());
+      restTemplate.setInterceptors(interceptors);
     }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper om =
-            Jackson2ObjectMapperBuilder
-                .json()
-                .build();
-        om.setSerializationInclusion(Include.NON_NULL);
-        return om;
+    restTemplate.setErrorHandler(new RestErrorHandler());
+
+    return restTemplate;
+  }
+
+  public static class RestErrorHandler extends DefaultResponseErrorHandler {
+
+    @Override
+    protected boolean hasError(HttpStatus statusCode) {
+      return statusCode.is4xxClientError() || statusCode.is5xxServerError();
     }
 
-    @Bean
-    public RestTemplate restTemplate(ObjectMapper om) {
-        RestTemplate restTemplate = new RestTemplate();
-        MappingJackson2HttpMessageConverter jacksonConverter =
-            new MappingJackson2HttpMessageConverter(om);
-        restTemplate.setMessageConverters(
-            restTemplate.getMessageConverters()
-                .stream()
-                .map(c -> c instanceof MappingJackson2HttpMessageConverter ? jacksonConverter : c)
-                .collect(Collectors.toList()));
-
-        if (env.acceptsProfiles("debug")) {
-            List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
-            if (interceptors == null || interceptors.isEmpty()) {
-                interceptors = new ArrayList<>();
-            }
-            interceptors.add(new LoggingInterceptor());
-            restTemplate.setInterceptors(interceptors);
-        }
-
-        restTemplate.setErrorHandler(new RestErrorHandler());
-
-        return restTemplate;
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+      throw new RuntimeException(
+          String.format(
+              "Error status code %d: %s",
+              response.getRawStatusCode(),
+              IOUtils.toString(response.getBody())));
     }
+  }
 
-    public static class RestErrorHandler extends DefaultResponseErrorHandler {
+  public static class LoggingInterceptor implements ClientHttpRequestInterceptor {
 
-        @Override
-        protected boolean hasError(HttpStatus statusCode) {
-            return statusCode.is4xxClientError() || statusCode.is5xxServerError();
-        }
+    private static Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
 
-        @Override
-        public void handleError(ClientHttpResponse response) throws IOException {
-            throw new RuntimeException(
-                String.format(
-                    "Error status code %d: %s",
-                    response.getRawStatusCode(),
-                    IOUtils.toString(response.getBody())));
-        }
+    @Override
+    public ClientHttpResponse intercept(
+        HttpRequest req,
+        byte[] reqBody,
+        ClientHttpRequestExecution ex)
+        throws IOException {
+      LOGGER.info("Request body: {}", new String(reqBody, StandardCharsets.UTF_8));
+      ClientHttpResponse response = ex.execute(req, reqBody);
+      InputStreamReader isr =
+          new InputStreamReader(response.getBody(), StandardCharsets.UTF_8);
+      String body = new BufferedReader(isr).lines().collect(Collectors.joining("\n"));
+      LOGGER.info("Response body: {}", body);
+      return response;
     }
-
-    public static class LoggingInterceptor implements ClientHttpRequestInterceptor {
-
-        private static Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
-
-        @Override
-        public ClientHttpResponse intercept(
-            HttpRequest req,
-            byte[] reqBody,
-            ClientHttpRequestExecution ex)
-            throws IOException {
-            LOGGER.info("Request body: {}", new String(reqBody, StandardCharsets.UTF_8));
-            ClientHttpResponse response = ex.execute(req, reqBody);
-            InputStreamReader isr =
-                new InputStreamReader(response.getBody(), StandardCharsets.UTF_8);
-            String body = new BufferedReader(isr).lines().collect(Collectors.joining("\n"));
-            LOGGER.info("Response body: {}", body);
-            return response;
-        }
-    }
+  }
 }
